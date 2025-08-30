09e42e3dfaed19b73cd22df7f3114c75b924ac5d
Raju <rajukonde80@gmail.com>
2025-08-30 11:07:47 +0530

    build: Configure project to build WAR file for Tomcat deployment

--- src/main/java/com/example/librarymanagement/LibrarymanagementApplication.java
+++ src/main/java/com/example/librarymanagement/LibrarymanagementApplication.java
@@ -2,12 +2,19 @@ package com.example.librarymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder; // <-- Add this import
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- Add this import
 
 @SpringBootApplication
-public class LibrarymanagementApplication {
+public class LibrarymanagementApplication extends SpringBootServletInitializer { // <-- Change this line
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibrarymanagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarymanagementApplication.class, args);
+    }
 
+    // ADD THIS METHOD
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LibrarymanagementApplication.class);
+    }
 }
